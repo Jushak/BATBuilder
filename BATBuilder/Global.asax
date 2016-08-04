d07faece7c452ff47d805b64c086ad4c8c27c936
Jussi Eerio <jussi.eerio@gmail.com>
2016-08-04 18:38:10 +0300

    Add project files.

---
+++ BATBuilder/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BATBuilder.MvcApplication" Language="C#" %>
---
+++ BATBuilder/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BATBuilder.Startup))]
+namespace BATBuilder
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
